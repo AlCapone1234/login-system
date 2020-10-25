6533d0ecd59ac66e2089f2588339cbb659d10ca9
AlCapone1234 <63857498+AlCapone1234@users.noreply.github.com>
2020-10-25 12:27:55 +0100

    Removed vars that I don't use.

--- cMain.h
+++ cMain.h
@@ -12,8 +12,6 @@ public:
 	wxButton* m_button1 = nullptr;
 	wxTextCtrl* username = nullptr;
 	wxTextCtrl* password = nullptr;
-	wxListBox* m_list1 = nullptr;
-	wxFrame* m_frame2 = nullptr;
 
 
 	void OnButtonClicked(wxCommandEvent &evt);
